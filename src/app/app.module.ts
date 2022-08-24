81e23117ebac9469742e2d872b7a6577ed36fbb6
SolankiDaxesh <solankidaxesh912@gmail.com>
2022-08-24 10:47:09 +0530

    5 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing/app-routing.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { HomeComponent } from './home/home.component';
-
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { CalendarModule } from 'primeng/calendar';
+import { SliderModule } from 'primeng/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from './service/user.service';
@@ -26,6 +29,10 @@ import { UserService } from './service/user.service';
     // BrowserAnimations Modules
     BrowserAnimationsModule,
     //  PrimeNG  Modules
+    ButtonModule,
+    TableModule,
+    CalendarModule,
+    SliderModule,
     //  Angular Form Modules
     FormsModule,
     ReactiveFormsModule,
